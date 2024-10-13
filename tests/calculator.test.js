c590b73b82a0f74de93999eafb152d6e1fc5633b
Claryng <thuytiennguyen140103@gmail.com>
2024-10-13 03:09:06 -0400

    Add test for cosine function

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -17,3 +17,9 @@ describe('Calculator Calculation Functions', function() {
 
 
 // Add your tests here
+describe('Calculator Cosine Function', function() {
+    const res = calculator.cosine(Math.PI);
+    it('should return -1', function() {
+        assert.strictEqual(res, -1);
+    });
+})
